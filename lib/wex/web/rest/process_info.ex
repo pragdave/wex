11b770b674c436a43958df8fb447c406e8fba620
James Fish <james@fishcakez.com>
2014-08-22 19:30:28 +0100

    Support :proc_lib initial calls

--- lib/wex/web/rest/process_info.ex
+++ lib/wex/web/rest/process_info.ex
@@ -9,10 +9,18 @@ defmodule Wex.Web.Rest.ProcessInfo do
     |> String.to_char_list
     |> :erlang.list_to_pid
     |> Process.info
+    |> translate_initial_call
     |> add_types
   end
 
   defp strip_elixir_inspect_prefix("#PID" <> pid), do: pid
   defp strip_elixir_inspect_prefix(pid),           do: pid
+
+  defp translate_initial_call(info) do
+    Keyword.update!(info, :initial_call,
+      fn({:proc_lib, :init_p, _}) -> :proc_lib.translate_initial_call(info)
+        (mfa)                     -> mfa
+      end)
+  end
 
 end
