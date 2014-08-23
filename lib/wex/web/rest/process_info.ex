8b3eb1eb4b0e750ccb5157762c865bdc8a7d4e74
James Fish <james@fishcakez.com>
2014-08-23 10:31:33 +0100

    Support remote process info

--- lib/wex/web/rest/process_info.ex
+++ lib/wex/web/rest/process_info.ex
@@ -8,13 +8,27 @@ defmodule Wex.Web.Rest.ProcessInfo do
     |> strip_elixir_inspect_prefix
     |> String.to_char_list
     |> :erlang.list_to_pid
-    |> Process.info
+    |> process_info
     |> translate_initial_call
     |> add_types
   end
 
   defp strip_elixir_inspect_prefix("#PID" <> pid), do: pid
   defp strip_elixir_inspect_prefix(pid),           do: pid
+
+  defp process_info(pid) do
+    try do
+     :rpc.pinfo(pid)
+    else
+      info when is_list(info) ->
+        info
+      _other ->
+        nil
+    catch
+      :exit, _ ->
+        nil
+    end
+  end
 
   defp translate_initial_call(info) do
     Keyword.update!(info, :initial_call,
